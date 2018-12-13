1c285b66e4a9297c5d4f489d4109c50dcc44bef0
Mohamed-Ramadan <mohamed_r_2015@yahoo.com>
2018-12-13 15:28:10 +0200

    updating travis yml files

--- add-keys.sh
+++ add-keys.sh
@@ -7,5 +7,5 @@ security import ./dist.cer -k ~/Library/Keychains/ios-build.keychain -A /usr/bin
 security import ./dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "approcks123456789" -A /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./dist.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./TravisDestProfile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
--- decryptCertificates.sh
+++ decryptCertificates.sh
@@ -1,3 +1,3 @@
 openssl aes-256-cbc -k "" -in dist.p12.enc -d -a -out dist.p12
 openssl aes-256-cbc -k "" -in dist.cer.enc -d -a -out dist.cer
-openssl aes-256-cbc -k "" -in dist.mobileprovision.enc -d -a -out dist.mobileprovision
+openssl aes-256-cbc -k "" -in TravisDestProfile.mobileprovision.enc -d -a -out TravisDestProfile.mobileprovision
