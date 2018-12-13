38186761880df0c007a6c4777817912c4afa91a5
Mohamed-Ramadan <mohamed_r_2015@yahoo.com>
2018-12-13 17:26:49 +0200

    sdfsdfsFS

---
+++ set-environment.sh
@@ -0,0 +1,11 @@
+secure=""
+HOCKEY_APP_ID=""
+HOCKEY_APP_TOKEN=""
+APP_NAME="DemoCI"
+XCODE_SCHEME="DemoCI"
+UNIT_TEST_SCHEME="DemoCITests"
+'DEVELOPER_NAME="iOS Distribution"'
+PROJECT_DIRECTORY="/"
+SCRIPT_FOLDER="travis-scripts"
+CERT_FOLDER="ios-certs"
+INFO_PLIST="DemoCI/Info.plist"
