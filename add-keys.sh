88fb72ccd567e88c8f39af1690d687a02419f718
Mohamed-Ramadan <mohamed_r_2015@yahoo.com>
2018-12-18 12:45:51 +0200

    updating profision profile name

--- add-keys.sh
+++ add-keys.sh
@@ -4,8 +4,8 @@ security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security import ./AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -A /usr/bin/codesign
 security import ./dist.cer -k ~/Library/Keychains/ios-build.keychain -A /usr/bin/codesign
-security import ./dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "approcks123456789" -A /usr/bin/codesign
+security import ./dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "" -A /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./TravisDestProfile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./dist.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
